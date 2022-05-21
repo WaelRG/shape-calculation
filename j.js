87aec312080299934be28e9b15725e579d0b8dad
WaelRG <100131161+WaelRG@users.noreply.github.com>
2022-05-21 09:31:20 -0600

    Update j.js

--- j.js
+++ j.js
@@ -20,3 +20,21 @@ function circumference(test2) {
         f.t3.value = "error";
     }
 }
+function art()
+{
+    var x = f.t.value * 1;
+      var y = f.t1.value * 1;
+    var button = document.getElementById("button");
+    var canves = document.getElementById("canves");
+    var ctx = canves.getContext('2d');
+    ctx.fillStyle = "rgb(0,51,51)";
+    ctx.fillRect(30, 30, x, y); 
+    ctx.stroke();
+}
+function cleare()
+{
+  var cl = document.getElementById("clear");
+  var canves = document.getElementById("canves");
+  var ctx = canves.getContext('2d');
+  ctx.clearRect(0, 0, canves.width, canves.height);
+}
