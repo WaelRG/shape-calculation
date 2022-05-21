3000239a6c5677b429887d7ce29bfadd610b1e1e
WaelRG <100131161+WaelRG@users.noreply.github.com>
2022-05-21 09:32:25 -0600

    Update j2js.js

--- j2js.js
+++ j2js.js
@@ -1,3 +1,5 @@
+
+
 function aria(test) {
     
     var r = f.t.value * 1;
@@ -8,10 +10,26 @@ function aria(test) {
   
 }
 function circumference(test2) {
-    var r = f.t.value * 1;
+      var r = f.t.value * 1;
     const p = 3.14;
     if (test2 == 2)
         f.t2.value = 2 * p * r;
     
     
+}
+function art()
+{
+      var r = f.t.value * 1;
+    var button = document.getElementById("button");
+    var canves = document.getElementById("canves");
+    var ctx = canves.getContext('2d');
+    ctx.arc(100 , 75 , r, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+function cleare()
+{
+  var cl = document.getElementById("clear");
+  var canves = document.getElementById("canves");
+  var ctx = canves.getContext('2d');
+  ctx.clearRect(0, 0, canves.width, canves.height);
 }
